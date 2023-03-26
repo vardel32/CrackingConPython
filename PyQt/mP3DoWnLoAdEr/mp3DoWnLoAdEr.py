7e24e55ca7431755c4118e4d3019ea08e8ed80b3
Vardel <36172712@uaz.edu.mx>
2023-03-26 14:54:56 -0600

    El código fuente de una intterfaz de usuario para descargar audo MP4 desde YouTube

---
+++ PyQt/mP3DoWnLoAdEr/mp3DoWnLoAdEr.py
@@ -0,0 +1,48 @@
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import *
+from pytube import YouTube
+
+
+class MainApp(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainApp, self).__init__(parent=parent)
+
+        # Se establece el tamanio de la ventana
+        self.setFixedSize(300, 300)
+        self.setWindowTitle("mp3 DoWnLoAdEr")
+
+        # Boton
+        self.button = QPushButton("Descargar MP4", self)
+        self.button.setGeometry(50, 120, 200, 30)
+        self.button.clicked.connect(self.descargar)
+
+        # Entrada de texto
+        self.text_input = QLineEdit(self)
+        self.text_input.setGeometry(50, 80, 200, 30)
+
+        # Labels
+        self.label_url = QLabel("URL", self)
+        self.label_url.setGeometry(135, 40, 200, 30)
+        self.label_estado = QLabel("", self)
+        self.label_estado.setGeometry(20, 150, 200, 30)
+
+        self.label_firma = QLabel("- insoportable -", self)
+        self.label_firma.setGeometry(110, 230, 200, 30)
+
+
+    def descargar(self):
+        url = self.text_input.text()
+        mp3 = YouTube(url).streams.get_audio_only()
+        mp3.download()
+        self.label_estado.setText(mp3.default_filename)
+
+
+if __name__ == '__main__':
+    # Inicia la aplicacion
+    app = QApplication([])
+    # Crea la ventana
+    window = MainApp()
+    # Muestra la ventana
+    window.show()
+    # Ejecuta la aplicacion
+    app.exec_()
